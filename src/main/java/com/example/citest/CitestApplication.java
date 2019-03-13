51ddd4f81bc52aecdb881e0aeccdc563dc57a84b
atsushi koshiba <koshiba@atsushinoMacBook-Pro.local>
2019-03-13 15:45:22 +0900

    initial commit

---
+++ src/main/java/com/example/citest/CitestApplication.java
@@ -0,0 +1,13 @@
+package com.example.citest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CitestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CitestApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/citest/HelloService.java
@@ -0,0 +1,11 @@
+package com.example.citest;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class HelloService {
+
+    public String sayHello(){
+        return "Hello World";
+    }
+}
