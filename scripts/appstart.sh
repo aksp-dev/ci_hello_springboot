4c301418cd29e782f506032c0f7504c33415e92a
atsushi koshiba <koshiba@atsushinoMacBook-Pro.local>
2019-03-25 12:10:04 +0900

    change buildspec.yml sh

--- scripts/appstart.sh
+++ scripts/appstart.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-
-java -jar /opt/app/target/citest-0.0.1-SNAPSHOT.jar >> /opt/app/target/citest-0.0.1-SNAPSHOT.log \ 2>&1 &
+sudo chmod +x /opt/app/target/citest-0.0.1-SNAPSHOT.jar
+echo 'start'
+#java -jar /opt/app/target/citest-0.0.1-SNAPSHOT.jar >> /opt/app/target/citest-0.0.1-SNAPSHOT.log \ 2>&1 &
